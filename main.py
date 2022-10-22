05e4fa5cb3f993a7a4fc0b5f9f18286a536d3ada
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-10-21 17:09:06 -0700

    Adding Function to Save Participant Data in data folder

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from pathlib import Path
+import datetime
 import time
 
 intro_file = Path("instructions.txt")
 outro_file = Path("goodbye.txt")
+data_dir = Path("data")
+
+if not data_dir.exists():
+    data_dir.mkdir(parents=True, exist_ok=True)
 
 
 with intro_file.open() as f:
@@ -11,10 +16,18 @@ with intro_file.open() as f:
 with outro_file.open() as f:
     debreif = f.readline()
 
+def save_data(data: str) -> None:
+    today = datetime.datetime.now()
+    filename = f"{today.strftime('%m-%d-%Y_%H-%M')}.csv"
+    data_file = data_dir / Path(filename)
+    with data_file.open("w") as f:
+        f.write(data)
+
 def main():
     print(instructions)
     time.sleep(1)
     name = input("Enter Name:")
     print("Hello " + name)
+    save_data(name)
     time.sleep(1)
     print(debreif)
